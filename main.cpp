01bed4038e5c4f41fa585d1aa61d360cdcb262af
yattew <yathansh@gmail.com>
2021-11-22 23:50:31 +0530

    reading and interpreting code from .sloth file

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "interpreter.h"
 using namespace std;
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     Interpreter I;
     if (argc == 1)
@@ -16,5 +16,23 @@ int main(int argc, char **argv)
             I.interpret(in);
         }
     }
-    else;
+    else if (argc == 2)
+    {
+        string name = "";
+        int idx = 0;
+        while(argv[1][idx]!='\0')
+        {
+            name.push_back(argv[1][idx]);
+            idx++;
+        }
+        ifstream file(name);
+        string contents;
+        string temp;
+        while(getline(file,temp))
+        {
+            contents.append(temp);
+            contents.push_back(' ');
+        }
+        I.interpret(contents);
+    }
 }
